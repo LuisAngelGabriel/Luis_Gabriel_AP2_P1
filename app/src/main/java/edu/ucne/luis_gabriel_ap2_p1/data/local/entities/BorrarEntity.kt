b541ac2d1a6e0e3b0ed8172c8ba2b23db7cc6965
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-04 18:15:20 -0600

    Agregando imports a la clase entity

---
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/local/entities/BorrarEntity.kt
@@ -0,0 +1,9 @@
+package edu.ucne.luis_gabriel_ap2_p1.data.local.entities
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName ="borrame")
+data class BorrarEntity(
+    @PrimaryKey(autoGenerate = true)
+
+)
