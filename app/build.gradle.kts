f153fd7b414f34374c5f140e8d518b6ef7f65a18
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-04 18:13:40 -0600

    Agregando Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
 }
 
 android {
@@ -39,22 +43,47 @@ android {
     buildFeatures {
         compose = true
     }
-}
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+        implementation(libs.androidx.compose.ui)
+        implementation(libs.androidx.compose.ui.graphics)
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.material3)
+
+        // Navegación
+        implementation("androidx.navigation:navigation-compose:2.8.5")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+        // Room
+        implementation("androidx.room:room-runtime:2.7.2")
+        implementation("androidx.room:room-ktx:2.7.2")
+
+        ksp("androidx.room:room-compiler:2.7.2")
+
+        implementation("androidx.compose.material:material-icons-extended:1.7.6")
+
+        // Hilt
+        implementation("com.google.dagger:hilt-android:2.57.1")
+        ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+        implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.11.0")
+        implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+        implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+        // Testing
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+        debugImplementation(libs.androidx.compose.ui.tooling)
+        debugImplementation(libs.androidx.compose.ui.test.manifest)
+    }
 }
