71a2e0d810b06f8fb857c3af49d01f3e0bd06c1e
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-04 18:31:58 -0600

    Creando la database

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/BorrarDB.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/BorrarDB.kt
@@ -1,4 +1,16 @@
 package edu.ucne.luis_gabriel_ap2_p1.data.database
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.luis_gabriel_ap2_p1.data.local.dao.BorrarDao
+import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.BorrarEntity
 
-class BorrarDB {
-}
+
+
+    @Database(
+        entities = [BorrarEntity::class],
+        version = 1,
+        exportSchema = false)
+    abstract class BorrarDB: RoomDatabase(){
+
+        abstract fun borrarDao(): BorrarDao
+    }
