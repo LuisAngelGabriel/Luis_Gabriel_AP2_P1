830ba2096d723758a3806529b8ada46899d00a59
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-04 18:41:48 -0600

    Agregando imports al app module

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/di/AppModule.kt
@@ -1,4 +1,11 @@
 package edu.ucne.luis_gabriel_ap2_p1.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
+
 }
