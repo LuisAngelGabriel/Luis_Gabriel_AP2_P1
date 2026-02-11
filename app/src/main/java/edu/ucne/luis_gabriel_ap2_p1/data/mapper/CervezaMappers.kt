af6a6766dd5d19a8bc351445cdd53f04ab525c30
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 17:04:44 -0600

    Creando el CervezaMappers

---
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/mapper/CervezaMappers.kt
@@ -0,0 +1,18 @@
+package edu.ucne.luis_gabriel_ap2_p1.data.mapper
+
+import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.CervezaEntity
+import edu.ucne.luis_gabriel_ap2_p1.domain.model.Cerveza
+
+fun CervezaEntity.toDomain(): Cerveza = Cerveza(
+    IdCerveza = IdCerveza,
+    nombre = nombre,
+    marca = marca,
+    puntuacion =puntuacion
+)
+
+fun Cerveza.toEntity(): CervezaEntity = CervezaEntity(
+    IdCerveza = IdCerveza,
+    nombre = nombre,
+    marca = marca,
+    puntuacion = puntuacion
+)
