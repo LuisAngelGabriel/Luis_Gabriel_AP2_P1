7a8cfae3a07a36b18ab8c04c769f3f765abb27df
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 16:18:30 -0600

    iniciando examen

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/BorrarDB.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/BorrarDB.kt
@@ -2,15 +2,15 @@ package edu.ucne.luis_gabriel_ap2_p1.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.luis_gabriel_ap2_p1.data.local.dao.BorrarDao
-import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.BorrarEntity
+//import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.BorrarEntity
 
 
 
-    @Database(
-        entities = [BorrarEntity::class],
-        version = 1,
-        exportSchema = false)
-    abstract class BorrarDB: RoomDatabase(){
+  //  @Database(
+    //    entities = [BorrarEntity::class],
+      //  version = 1,
+        //exportSchema = false)
+  //  abstract class BorrarDB: RoomDatabase(){
 
-        abstract fun borrarDao(): BorrarDao
-    }
+    //    abstract fun borrarDao(): BorrarDao
+   // }
