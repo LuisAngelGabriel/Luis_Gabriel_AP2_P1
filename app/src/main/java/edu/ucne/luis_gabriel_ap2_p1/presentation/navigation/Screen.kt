aa464a238a81ba132b276f180bc7fb869ef01dcc
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 22:30:51 -0600

    Creand el DrawerMenu

---
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/presentation/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package edu.ucne.luis_gabriel_ap2_p1.presentation.navigation
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+
+    @Serializable
+    data object CervezaList : Screen()
+
+    @Serializable
+    data class CervezaEdit(val cervezaId: Int) : Screen()
+}
