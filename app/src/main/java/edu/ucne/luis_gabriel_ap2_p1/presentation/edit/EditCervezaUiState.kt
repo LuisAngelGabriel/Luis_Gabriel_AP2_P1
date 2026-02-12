6fa311fcbffbd5ed904547411930ec5123f2d23d
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 21:15:37 -0600

    Agregando datos al EditCervezaUiState

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/presentation/edit/EditCervezaUiState.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/presentation/edit/EditCervezaUiState.kt
@@ -1,4 +1,18 @@
 package edu.ucne.luis_gabriel_ap2_p1.presentation.edit
 
-class EditCervezaUiState {
-}
+data class EditCervezaUiState(
+    val IdCerveza: Int? = null,
+    val nombre: String = "",
+    val marca: String = "",
+    val puntuacion: String = "",
+    val nombreError: String? = null,
+    val marcaError: String? = null,
+    val puntuacionError: String? = null,
+    val isLoading: Boolean = false,
+    val isSaving: Boolean = false,
+    val isDeleting: Boolean = false,
+    val isNew: Boolean = true,
+    val saved: Boolean = false,
+    val deleted: Boolean = false,
+    val errorMessage: String? = null
+)
