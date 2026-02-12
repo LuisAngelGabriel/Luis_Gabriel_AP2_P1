230af137cbd21e698a4b4ea80232da320bc1f03b
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 21:13:53 -0600

    Creando e cervezaUiEvent

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/CervezaDB.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/data/database/CervezaDB.kt
@@ -4,8 +4,6 @@ import androidx.room.RoomDatabase
 import edu.ucne.luis_gabriel_ap2_p1.data.local.dao.CervezaDao
 import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.CervezaEntity
 
-
-
     @Database(
         entities = [CervezaEntity::class],
         version = 1,
--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/presentation/edit/EditCervezaUiEvent.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/presentation/edit/EditCervezaUiEvent.kt
@@ -1,4 +1,10 @@
 package edu.ucne.luis_gabriel_ap2_p1.presentation.edit
 
-interface EditCervezaUiEvent {
+sealed interface EditCervezaUiEvent {
+    data class Load(val id: Int?) : EditCervezaUiEvent
+    data class NombreChanged(val value: String) : EditCervezaUiEvent
+    data class MarcaChanged(val value: String) : EditCervezaUiEvent
+    data class PuntuacionChanged(val value: String) : EditCervezaUiEvent
+    data object Save : EditCervezaUiEvent
+    data object Delete : EditCervezaUiEvent
 }
