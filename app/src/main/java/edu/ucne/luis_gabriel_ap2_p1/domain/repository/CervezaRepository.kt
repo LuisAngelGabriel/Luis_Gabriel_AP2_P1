77cdeb3393fe99cfdcec9c148d7fc7256592f522
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-02-11 20:48:29 -0600

    Corrigiendo la implementacion de repositorio, cambiando cerveza entity por cerveza

--- app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/domain/repository/CervezaRepository.kt
+++ app/src/main/java/edu/ucne/luis_gabriel_ap2_p1/domain/repository/CervezaRepository.kt
@@ -1,13 +1,12 @@
 package edu.ucne.luis_gabriel_ap2_p1.domain.repository
 
-import edu.ucne.luis_gabriel_ap2_p1.data.local.entities.CervezaEntity
+import edu.ucne.luis_gabriel_ap2_p1.domain.model.Cerveza
 import kotlinx.coroutines.flow.Flow
+
 interface CervezaRepository {
-    fun observeAll(): Flow<List<CervezaEntity>>
-    suspend fun upsert(cerveza: CervezaEntity): Long
-    suspend fun getById(id: Int): CervezaEntity?
-    suspend fun delete(cerveza: CervezaEntity)
-    fun observeFiltered(nombre: String?): Flow<List<CervezaEntity>>
+    fun observeAll(): Flow<List<Cerveza>>
+    suspend fun upsert(cerveza: Cerveza): Int
+    suspend fun getById(id: Int): Cerveza?
+    suspend fun delete(id: Int)
+    fun observeFiltered(nombre: String?): Flow<List<Cerveza>>
 }
-
-
